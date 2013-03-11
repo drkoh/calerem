e978ba953eb53fdc823dca250038c481225b3145
pli1885@gmail.com <pli1885@gmail.com@e0817914-cf66-8ef2-6da0-c6d823da1cd8>
2013-03-11 23:31:17 +0000

    Revision: 29 - https://code.google.com/p/calerem/issues/detail?id=67&can=1&sort=status%20owner%20priority%20summary
    Revision: 31 - https://code.google.com/p/calerem/issues/detail?id=70&can=1&sort=status%20owner%20priority%20summary
    
    c_sync.java - replaced "c_facebook_API" with "c_Facebook_API" due to compilation error
    
    Revision: 32 - https://code.google.com/p/calerem/issues/detail?id=32&can=1&sort=status%20owner%20priority%20summary

--- branches/working/src/com/example/calerem/c_sync.java
+++ branches/working/src/com/example/calerem/c_sync.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class c_sync extends Activity{
 c_eortologio_API v_eortologio;
-c_facebook_API v_facebook;
+c_Facebook_API v_facebook;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
