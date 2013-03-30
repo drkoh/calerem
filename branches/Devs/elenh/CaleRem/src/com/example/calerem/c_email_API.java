cc3003d1c2166500571e70d88a7b54159d372518
elenaidon <elenaidon@e0817914-cf66-8ef2-6da0-c6d823da1cd8>
2013-03-30 12:41:18 +0000

    early MoveCopyEvent Activity developement

---
+++ branches/Devs/elenh/CaleRem/src/com/example/calerem/c_email_API.java
@@ -0,0 +1,12 @@
+package com.example.calerem;
+
+import android.app.Activity;
+
+public class c_email_API extends Activity
+{
+	
+	public void f_send_mail(String v_mail_text, String v_mail_address)
+	{
+		
+	}
+}
