a9563a20a3ad190f25dd080ff32418bd87100918
agapspyr <agapspyr@gmail.com>
2013-04-08 17:35:39 +0000

    EortologioApi.java written, it returns an ArrayList containing the Greek and English namedays generated from the XML gained from http://www.eortologio.gr/ . EortologioApiTest.java written witch is testing the EortologioApi.java, it is running 2 tests successfully. FacebookApi.java renamed and warnings removed.

---
+++ agapi/EortologioApiTest.java
@@ -0,0 +1,65 @@
+package com.example.calerem;
+
+import static org.junit.Assert.*;
+
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+public class EortologioApiTest {
+
+
+	private String xmlStrEn;
+	private String xmlStrEl;
+	private Document documentEn;
+	private Document documentEl;
+	
+	@Before
+	public void initVars() {
+		xmlStrEn = "http://www.eortologio.gr/rss/si_en.xml";
+		xmlStrEl = "http://www.eortologio.gr/rss/si_el.xml";
+		documentEn = generateXml(xmlStrEn);
+		documentEl = generateXml(xmlStrEl);
+	}
+	
+	private static Document generateXml (String str) {
+		try {
+			URL url = new URL(str);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(url.openStream());
+			return doc;
+		} catch (Exception ex) {
+			Document doc = null;
+			ex.printStackTrace();
+			return doc;
+		}
+	}
+
+	@Test
+	public void getXmlEnTest() {
+		/*
+		 * Generate English Namedays end compare XML
+		 */
+		EortologioApi ea = new EortologioApi();
+		Document doc = ea.getXml(xmlStrEn);
+		assertEquals(doc.toString(),documentEn.toString());
+	}
+	
+	@Test
+	public void getXmlElTest() {
+		/*
+		 * Generate Greek Namedays end compare XML
+		 */
+		EortologioApi ea = new EortologioApi();
+		Document doc = ea.getXml(xmlStrEl);
+		assertEquals(doc.toString(),documentEl.toString());
+	}
+
+
+}
