55e8b7a96fe753f10be4ad5857fea8a9724e2401
elenaidon <elenaidon@e0817914-cf66-8ef2-6da0-c6d823da1cd8>
2013-04-08 17:53:50 +0000

    FindContacts.java Input Contact ArrayList + Eortologio ArrayList Output ContactId with integers that point to the given Contact ArrayLists items

--- branches/Devs/FindContacts/FindContacts.java
+++ branches/Devs/FindContacts/FindContacts.java
@@ -5,23 +5,24 @@ import java.util.EmptyStackException;
 
 public class FindContacts {
 
-	ArrayList<Integer> searchNames(final String[] contactNames,
-			final String[] eortologioNames) {
+	ArrayList<Integer> searchNames(final ArrayList<Contact> contactNames,
+			final ArrayList<String> eortologioNames) {
 		final ArrayList<Integer> contactId = new ArrayList<Integer>();
 
 		// contactId.ensureCapacity(contactNames.length);
 
 		int contactCursor, eortologioCursor;
 		try {
-			if (contactNames.length > 0 && eortologioNames.length > 0) {
-				for (contactCursor = 0; contactCursor < contactNames.length; contactCursor++) {
-					for (eortologioCursor = 0; eortologioCursor < eortologioNames.length; eortologioCursor++) {
-						if ((contactNames[contactCursor]).equals(eortologioNames[eortologioCursor])) {
+			if (contactNames.size() > 0 && eortologioNames.size() > 0) {
+				for (contactCursor = 0; contactCursor < contactNames.size(); contactCursor++) {
+					for (eortologioCursor = 0; eortologioCursor < eortologioNames.size(); eortologioCursor++) {
+						if ((contactNames.get(contactCursor).name)
+								.equals(eortologioNames.get(eortologioCursor))) {
 							contactId.add(contactCursor);
 						}
 					}
 				}
-			}else{
+			} else {
 				throw new EmptyStackException();
 			}
 		} catch (Exception e) {
