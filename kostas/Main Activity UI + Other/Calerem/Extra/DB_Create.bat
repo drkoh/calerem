2eefea948ce1ffba18fd845686df4678821cac89
destiny_gs <destiny_gs@yahoo.gr>
2013-05-02 16:30:46 +0000

    Calerem.db = Updated to latest schema.
    commands.sql
    = Updated to latest schema.
    
    sqlite3.exe
    + Added so others can build the database too.
    
    layout/activity_main.xml
    + Main activity's UI
    + Lots of changes!
    
    layout/calendar_day_gridcell.xml
    + Represents 1 object in the gridcell.
    
    res/drawable
    + Added all the button icons for main activity.
    
    calendar_day_gridcell.xml
    + Added the strings needed by the ui. 
    
    MainActivity.xml
    + Added the strings needed by the ui.
    
    EortologioAPI.java
    + Change the path of download to cache directory (better).
    
    Celebration.java
    + New class to hold a Celebration.
    
    Database.java
    + Added a function to return a celebration based on day + name of the celebration. Used to validate duplicates.
    + Changes to reflect the new class added.
    
    SyncController.java
    + Added all needed steps to call the EortologioAPI and store the celebrations in the database.
    
    handlers
    + Created new classes to handle UI actions.
    
    MainActivity.java
    + Changes to reflect UI Changes.
    
    +++ A lot other that i forgot ^^ +++
    --- Tests are missing till i can talk with agaph ---

---
+++ kostas/Main Activity UI + Other/Calerem/Extra/DB_Create.bat
@@ -0,0 +1,4 @@
+cd "E:\To Archive\NewCalerem\Calerem\Extra"
+"E:\To Archive\NewCalerem\Calerem\Extra\sqlite3" "E:\To Archive\NewCalerem\Calerem\Extra\Calerem.db" < "E:\To Archive\NewCalerem\Calerem\Extra\commands.sql"
+
+
---
+++ kostas/Main Activity UI + Other/Calerem/src/com/calerem/factories/UIActivityResultsHandlerFactory.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.calerem.factories;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.calerem.handlers.NewEventResult;
+import com.calerem.handlers.SendEmailResult;
+
+/**
+ * Handler for all Form results.
+ * @author DarkParadise
+ */
+public class UIActivityResultsHandlerFactory {
+	private Context basecontext;
+
+	/**
+	 * Base Constructor.
+	 * @param Context context
+	 */
+	public UIActivityResultsHandlerFactory(Context context)
+	{
+		basecontext = context;
+	}
+	/**
+	 * Uses the request code to decide which form returned the result.
+	 * Check the inner comment block for all acceptable codes.
+	 * @param int requestCode
+	 * @param int resultCode
+	 * @param Intent data
+	 */
+	public void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		/* Request Codes
+		 * 1 = New Event
+		 * 2 = Send Email
+		 */
+		switch (requestCode)
+		{
+		case 1:
+			new NewEventResult(basecontext, resultCode, data);
+			break;
+		case 2:
+			new SendEmailResult(basecontext,resultCode, data);
+			break;
+		}
+	}
+}
