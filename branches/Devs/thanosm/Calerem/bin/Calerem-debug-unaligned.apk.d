0da6d014b41ae61ebdc4031515e6c97ba5ab4634
boilosthanos90@gmail.com <boilosthanos90@gmail.com@e0817914-cf66-8ef2-6da0-c6d823da1cd8>
2013-05-27 14:08:38 +0000

    Didnt know about having to commit the whole project. So now again i improved database test and fixed exceptions in database

---
+++ branches/Devs/thanosm/Calerem/bin/Calerem-debug-unaligned.apk.d
@@ -0,0 +1,5 @@
+C:\Users\AnastasiaSwthria\Desktop\Kwstas\trunk\Calerem\bin\Calerem-debug-unaligned.apk : \
+C:\Users\AnastasiaSwthria\Desktop\Kwstas\trunk\Calerem\bin\Calerem.ap_ \
+C:\Users\AnastasiaSwthria\Desktop\Kwstas\trunk\Calerem\bin\classes.dex \
+C:\Users\AnastasiaSwthria\Desktop\Kwstas\trunk\Calerem\libs\gson-2.2.2.jar \
+C:\Users\AnastasiaSwthria\Desktop\Kwstas\trunk\Calerem\libs\android-support-v4.jar \
